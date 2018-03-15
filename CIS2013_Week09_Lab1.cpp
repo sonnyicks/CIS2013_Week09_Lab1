ee3e9c91d62190ca7aef06de5fd967365cfad2fd
sonnyicks <35547157+sonnyicks@users.noreply.github.com>
2018-03-14 20:09:24 -0500

    Lab for week 9

---
+++ CIS2013_Week09_Lab1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	char name[6] = "Bobby";
+	char last[8] = "Rockers";
+	
+	cout << "First name: " << name << endl;
+	cout << "Last name: " << last << endl;
+	return 0;
+}
